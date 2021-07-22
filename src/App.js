03c819d1b76f16122d2745c47b48dd1092543882
Shareena Begum <shareena.fsp@gmail.com>
2021-07-22 21:42:30 +0200

    all react files added in components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import React from "react"
+
 import './App.css';
+import Header from "./components/Header.js"
+import MainContent from "./components/MainContent.js"
+import Footer from "./components/Footer.js"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <Header />
+    <MainContent />
+    <Footer />
     </div>
   );
 }
---
+++ src/components/MainContent.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+function MainContent(){
+    return(
+        <div>This is a MainContent</div>
+    )
+}
+
+export default MainContent
