92d51bfa02d013004918c1f15c6060369def29e6
Shareena Begum <shareena.fsp@gmail.com>
2021-07-23 21:42:59 +0200

    deleted files

--- src/components/Header.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-function Header(){
-    return(
-        <div className="navbar">This is a header</div>
-    )
-}
-
-export default Header
--- src/components/MainContent.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-function MainContent(){
-    return(
-        <div>This is a MainContent</div>
-    )
-}
-
-export default MainContent
